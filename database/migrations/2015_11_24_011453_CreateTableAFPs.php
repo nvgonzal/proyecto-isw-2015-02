cb3aeb18bee4c75c581d1cb79fa4933ed7e5b132
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-11-25 00:35:44 -0300

    Se crea migracion para crear tabla de AFP

---
+++ database/migrations/2015_11_24_011453_CreateTableAFPs.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAFPs extends Migration {
+
+	/**
+	 * Corre la migrancion para crear la tabla en la base de datos que contiene la informacion de las AFP existentes
+	 * en el sistema previsional chileno
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::dropIfExists('afps');//Elimina la tabla AFPs si existe.
+		Schema::create('afps', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('nombre',40);
+			$table->integer('telefono');
+			$table->string('email',50);
+			$table->string('link_envio',100);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Revierte la migracion de la tabla AFP
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('afps');
+	}
+
+}
