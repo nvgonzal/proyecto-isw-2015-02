aabc44fdabd4fa218d79a4ea1d52109ebbfe640f
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-11-25 16:21:59 -0300

    Se crea migracion para crear tabla de cargas legales

---
+++ database/migrations/2015_11_25_190907_CreateTableCargas.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCargas extends Migration {
+
+	/**
+	 * Corre la migracion para crear la tabla de cargas legales en la base de datos
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('cargas', function(Blueprint $table)
+		{
+			$table->string('rut',12);
+			$table->string('nombres',50);
+			$table->string('apellido_paterno',40);
+			$table->string('apellido_materno',40);
+			$table->date('fecha_nacimiento');
+			$table->string('parentesco',30);
+			$table->string('ocupacion',30);
+			$table->string('rut_empleado',12);
+			$table->timestamps();
+			$table->softDeletes();
+			//Definicion clave primaria
+			$table->primary('rut');
+			//Definicion claves foraneas
+			$table->foreign('rut_empleado')->references('rut')->on('empleados');
+		});
+	}
+
+	/**
+	 * Revierte la creacion de la tabla
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('cargas');
+	}
+
+}
