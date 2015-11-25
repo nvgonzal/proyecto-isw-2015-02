f01d5fdd1589d08c47bcc95e38e33c492f969a0d
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-11-25 17:21:05 -0300

    Se crea migracion para crear tabla de asistencia de trabajadores

---
+++ database/migrations/2015_11_25_195837_CreateTableAsistencias.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAsistencias extends Migration {
+
+	/**
+	 * Corre la migracion para crear la tabla de asistencias
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('asistencias', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->date('fecha');
+            $table->time('hora_ingreso');
+            $table->time('hora_salida');
+            $table->float('horas_extra');
+            $table->string('rut_empleado',12);
+			$table->timestamps();
+            $table->softDeletes();
+            //claves foraneas
+            $table->foreign('rut_empleado')->references('rut')->on('empleados');
+		});
+	}
+
+	/**
+	 * Revierte la creacion de la tabla asistencias.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('asistencias');
+	}
+
+}
