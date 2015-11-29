bf5f5494536d19f56afc54b92b628a0e9511b07f
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-11-29 01:39:45 -0300

    Creada clase seeder para tabla de Instituciones de salud previsional

---
+++ database/seeds/AseguradorasSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AseguradorasSeeder extends Seeder{
+
+    public function run(){
+        $faker = \Faker\Factory::create('es_ES');
+
+        $nombres = [
+            'FONASA',
+            'Banmedica S.A.',
+            'Chuquicamata Ltda.',
+            'Colmena Golden Cross S.A.',
+            'Consalud S.A.',
+            'Cruz Blanca S.A.',
+            'Cruz del Norte Ltda.',
+            'Optima S.A.',
+            'Fundacion Ltda.',
+            'Fusat Ltda.',
+            'Masvida S.A.',
+            'Rio Blanco Ltda.',
+            'San Lorenzo Ltda.',
+            'Vida Tres S.A.'
+        ];
+
+        for($i=0;$i<14;$i++){
+            $now = date('Y-m-d H:i:s');
+            DB::table('salud')->insert([
+                'nombre' => $nombres[$i],
+                'telefono' => $faker->phoneNumber,
+                'email' => $faker->companyEmail,
+                'link_envio' => $faker->unique()->ipv6,
+                'created_at' => $now,
+                'updated_at' => $now
+            ]);
+        }
+
+
+    }
+}
