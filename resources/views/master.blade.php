744e2143496370e9c07d97acc7f8e00a8fba01e8
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-12-06 17:37:14 -0300

    Agrega barra de navegacion para la pagina

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,16 +12,42 @@
     <body>
         <header>
         </header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="{!! URL::to('/home') !!}">Colegio Alba</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="{!! Url::to('empleados') !!}">Empleados <span class="sr-only">(current)</span></a></li>
+                        <li><a href="{!! Url::to('asistencia') !!}">Asistencia</a></li>
+                        <li><a href="{!! URL::to('liquidaciones') !!}">Liquidaciones</a></li>
+                    </ul>
+                    <!--Barra para buscar-->
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Cerrar sesion</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
         <div class="container">
             @yield('contenido')
         </div>
         <footer class="panel-footer">
-            <div class="container">
+            <div class="text-center">
                 <span class="label label-info">Pagina creada para el ramo de ingenieria de software 2015</span>
             </div>
         </footer>
         {!! Html::script('js/bootstrap.js') !!}
-
     </body>
 
 </html>
