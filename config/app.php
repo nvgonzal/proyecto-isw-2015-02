e5ad2f4db8e2e2f1d1120adc343d3c7188e3ec65
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-12-06 17:10:58 -0300

    Agregadas dependencia para debug bar de laravel

--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 		'Collective\Html\HtmlServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider',
 
 	],
 
@@ -195,6 +196,7 @@ return [
 		'View'      => 'Illuminate\Support\Facades\View',
 		'Form' => 'Collective\Html\FormFacade',
 		'Html' => 'Collective\Html\HtmlFacade',
+		'Debugbar' => 'Barryvdh\Debugbar\Facade::class,'
 
 	],
 
