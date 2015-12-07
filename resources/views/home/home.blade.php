9b41d6104fc3ee45471ccd901b946148021150e1
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-12-07 16:59:26 -0300

    Crea vista principal de la pagina

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('titulo','Pagina principal')
+
+@section('contenido')
+    <div class="jumbotron">
+        <div class="container">
+            <h2>Bienvenido al sistema de calculo de sueldo de Colegio Alba</h2>
+        </div>
+    </div>
+@endsection
