d8924d7c226a4209fc3b8fa3f5213351be4e870b
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-12-08 17:27:50 -0300

    Cambiado texto del inicio de la web

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,7 +5,7 @@
 @section('contenido')
     <div class="jumbotron">
         <div class="container">
-            <h2>Bienvenido al sistema de calculo de sueldo de Colegio Alba</h2>
+            <h2>Bienvenido al sistema administrativo del Colegio Alba</h2>
         </div>
     </div>
 @endsection
