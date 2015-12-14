3258e099268edb4c524eb4d83312f539d1571806
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-12-14 11:16:30 -0300

    Agrega descripcion a la pagina

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Pagina creada para el ramo de ingenieria de software">
         {!! Html::style('css/bootstrap.css') !!}
         <title>@yield('titulo') | Colegio Alba</title>
 
