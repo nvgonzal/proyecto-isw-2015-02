87def3e000fe154174a821f226f68e45eafb749f
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2015-12-30 15:01:52 -0300

    Arreglados problemas de indexado

--- resources/views/AFP/index.blade.php
+++ resources/views/AFP/index.blade.php
@@ -4,34 +4,31 @@
 
 @section('contenido')
     <a class="btn btn-success boton-fixed btn-lg" data-toggle="tooltip" title="Agregar AFP"
-       href="{!! URL::to('AFP/create') !!}">
+       href="{!! URL::to('afp/create') !!}">
         <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
     </a>
     <table class="table table-striped">
-        <th>Rut</th>
+        <th>ID</th>
         <th>Nombre</th>
         <th>Email</th>
         <th>Telefono</th>
         <th>Link</th>
-        @foreach($AFP as $AFP)
+        <th>Acciones</th>
+        @foreach($AFPs as $AFP)
             <tr>
-                <td>{{$AFP->rut}}</td>
-                <td>{{$AFP->nombres}}</td>
+                <td>{{$AFP->id}}</td>
+                <td>{{$AFP->nombre}}</td>
                 <td>{{$AFP->email}}</td>
                 <td>{{$AFP->telefono}}</td>
-                <td>{{$AFP->link}}</td>
+                <td>{{$AFP->link_envio}}</td>
                 <td>
-                    <a class="btn btn-default" data-toggle="tooltip" title="Informacion detallada"
-                       href="{!! URL::to('AFP/'.$AFP->rut) !!}">
-                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                    </a>
                     </a>
                     <a class="btn btn-warning btn-sm" data-toggle="tooltip" title="Editar informacion"
-                       href="{{ URL::to('AFP/'.$AFP->rut.'/edit') }}">
+                       href="{{ URL::to('afp/'.$AFP->rut.'/edit') }}">
                         <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
                     </a>
                     <a class="btn btn-danger btn-sm" data-toggle="tooltip" title="Eliminar AFP"
-                       href="{{ route('AFP.destroy',$AFP->rut) }}" onclick="alert('¿Seguro que quiere eliminar AFP?' +
+                       href="{{ route('afp.destroy',$AFP->rut) }}" onclick="alert('¿Seguro que quiere eliminar AFP?' +
                             '\nPuedes recuperarlo despues');">
                         <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
                     </a>
@@ -39,6 +36,6 @@
             </tr>
         @endforeach
     </table>
-    <div class="text-center"> {!! $AFP->render() !!}</div>
+    <div class="text-center"> {!! $AFPs->render() !!}</div>
 
 @endsection
