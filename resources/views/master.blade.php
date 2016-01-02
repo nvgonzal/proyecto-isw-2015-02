69dcb6370cc98d58bf649ba78b5c70f8db51c9a6
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-01-02 17:46:39 -0300

    Agrega botones para agregar afps e isapres al sistema. Agrega boton para mostrar empleados desvinculados

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -15,7 +15,7 @@
     <body>
         <header style="background-color: #9e0b37">
             <div class="container" align="center">
-                {!! Html::image(URL::to('img/banner.jpg'),'Banner',['class'=>'img-responsive'],null) !!}
+                {!! Html::image('img/banner.jpg','Banner',['class'=>'img-responsive'],null) !!}
             </div>
         </header>
         <nav class="navbar navbar-default">
@@ -23,7 +23,7 @@
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
                     <a class="navbar-brand">
-                        {!! HTML::image(URL::to('img/logo.png'),'logo.png',['class'=>'img-responsive repon']) !!}
+                        {!! HTML::image('img/logo.png','logo.png',['class'=>'img-responsive repon']) !!}
                     </a>
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                         <span class="sr-only">Toggle navigation</span>
@@ -37,14 +37,36 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li><a href="{!! Url::to('empleados') !!}">Empleados <span class="sr-only">(current)</span></a></li>
+                        <li class="dropdown">
+                            <a href="{!! Url::to('empleados') !!}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Empleados<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Empleados desvinculados</a></li>
+                            </ul>
+                        </li>
                         <li><a href="{!! Url::to('asistencia') !!}">Asistencia</a></li>
                         <li><a href="{!! URL::to('liquidaciones') !!}">Liquidaciones</a></li>
                     </ul>
                     <!--Barra para buscar-->
                     <ul class="nav navbar-nav navbar-right">
-                        <!--<li><a href="#">Registrar cuenta</a></li>
-                        <li><a href="#">Cerrar sesion</a></li>-->
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu gestion<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{URL::to('afp')}}">Gestion AFP</a></li>
+                                <li><a href="#">Gestion Instituto de salud</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="{{URL::to('auth/register')}}">Registrar cuenta</a></li>
+                        @if(Auth::check())
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                                   role="button"
+                                   aria-haspopup="true" aria-expanded="false">{{ Auth::user()->rut }}
+                                    <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="{{ URL::to('auth/logout') }}">Cerrar Sesión</a></li>
+                                </ul>
+                            </li>
+                        @endif
                     </ul>
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
