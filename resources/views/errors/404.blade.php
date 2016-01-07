4f866584279daa328125a6ceefa9d55662fe2795
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-01-07 13:11:45 -0300

    Crea vista de error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,36 @@
+@extends('master-no-nav')
+
+@section('titulo','Pagina no encontrada')
+
+@section('estilos')
+    <style>
+        body { background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAYAAACpSkzOAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAABZ0RVh0Q3JlYXRpb24gVGltZQAxMC8yOS8xMiKqq3kAAAAcdEVYdFNvZnR3YXJlAEFkb2JlIEZpcmV3b3JrcyBDUzVxteM2AAABHklEQVRIib2Vyw6EIAxFW5idr///Qx9sfG3pLEyJ3tAwi5EmBqRo7vHawiEEERHS6x7MTMxMVv6+z3tPMUYSkfTM/R0fEaG2bbMv+Gc4nZzn+dN4HAcREa3r+hi3bcuu68jLskhVIlW073tWaYlQ9+F9IpqmSfq+fwskhdO/AwmUTJXrOuaRQNeRkOd5lq7rXmS5InmERKoER/QMvUAPlZDHcZRhGN4CSeGY+aHMqgcks5RrHv/eeh455x5KrMq2yHQdibDO6ncG/KZWL7M8xDyS1/MIO0NJqdULLS81X6/X6aR0nqBSJcPeZnlZrzN477NKURn2Nus8sjzmEII0TfMiyxUuxphVWjpJkbx0btUnshRihVv70Bv8ItXq6Asoi/ZiCbU6YgAAAABJRU5ErkJggg==);}
+        .error-template {padding: 40px 15px;text-align: center;}
+        .error-actions {margin-top:15px;margin-bottom:15px;}
+        .error-actions .btn { margin-right:10px; }
+    </style>
+
+    @stop
+
+@section('contenido')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-template">
+                    <h1>
+                        Oops!</h1>
+                    <h2>
+                        404</h2>
+                    <div class="error-details">
+                        ¡La pagina solicitada no se encuentra disponible!
+                    </div>
+                    <div class="error-actions">
+                        <a href="{{URL::to('home')}}" class="btn btn-primary"><span class="glyphicon glyphicon-home"></span>
+                            Volver a inicio </a><a href="#" class="btn btn-default btn"><span class="glyphicon glyphicon-envelope"></span> Contactar con el administrador </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@stop
