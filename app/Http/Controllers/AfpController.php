0ab53ff54547a573300dcc9c84d91c058693225a
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-01-18 00:06:10 -0300

    Agrega proteccion de rutas

--- app/Http/Controllers/AfpController.php
+++ app/Http/Controllers/AfpController.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Validator;
 
 class AfpController extends Controller {
 
+	public function __construct(){
+		$this->middleware('auth');
+	}
+
 	/**
 	 * Display a listing of the resource.
 	 *
